b870a04a96fea5afcb9271ef8a27534fd5251fc6
Sebatian Klüft <skluft@gmail.com>
2017-08-05 13:53:45 +0200

    New picker fragment with id

--- app/src/main/java/kesk/workhours/WorkDayActivity.kt
+++ app/src/main/java/kesk/workhours/WorkDayActivity.kt
@@ -4,22 +4,30 @@ import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.DialogFragment
 import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.DatePicker
 import android.widget.TimePicker
 import kotlinx.android.synthetic.main.activity_work_day.*
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 class WorkDayActivity : AppCompatActivity(),
         DatePickerDialog.OnDateSetListener,
-        TimePickerDialog.OnTimeSetListener {
+        TimePickedListener {
+
+    val WORK_DAY_DATE_PICKER = 1
+    val WORK_DAY_START_PICKER = 2
+    val LUNCH_START_PICKER = 3
+    val LUNCH_END_PICKER = 4
+    val WORK_DAY_END_PICKER = 5
 
     val dateFormater = SimpleDateFormat("yyyy-MM-dd")
     val timeFormater = SimpleDateFormat("HH:mm")
 
-    var workDayStart = Calendar.getInstance()
+    val workDayStart = Calendar.getInstance()
     var workDayEnd: Calendar? = null
 
     var lunchStart: Calendar? = null
@@ -31,12 +39,18 @@ class WorkDayActivity : AppCompatActivity(),
 
         datePickButton.text = dateFormater.format(workDayStart.time)
         datePickButton.setOnClickListener {
-            DatePickerFragment().show(fragmentManager, "datePicker")
+            //DatePickerFragment().show(fragmentManager, "datePicker")
         }
 
         workDayStartButton.text = timeFormater.format(workDayStart.time)
         workDayStartButton.setOnClickListener {
-            TimePickerFragment().show(fragmentManager, "timePicker")
+            TimePickerFragment.create(WORK_DAY_START_PICKER)
+                    .show(fragmentManager, "timePicker")
+        }
+
+        lunchStartButton.setOnClickListener {
+            TimePickerFragment.create(LUNCH_START_PICKER)
+                    .show(fragmentManager, "timePicker")
         }
     }
 
@@ -45,34 +59,66 @@ class WorkDayActivity : AppCompatActivity(),
         datePickButton.text = dateFormater.format(workDayStart.time)
     }
 
-    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        workDayStart.set(Calendar.HOUR_OF_DAY, hourOfDay)
-        workDayStart.set(Calendar.MINUTE, minute)
-        workDayStart.set(Calendar.SECOND, 0)
-        workDayStartButton.text = timeFormater.format(workDayStart.time)
+    override fun timePicked(id: Int, hourOfDay: Int, minute: Int) {
+        when (id) {
+            WORK_DAY_START_PICKER -> {
+                setTime(workDayStart, hourOfDay, minute)
+                workDayStartButton.text = timeFormater.format(workDayStart.time)
+            }
+
+            LUNCH_START_PICKER -> {
+                if (lunchStart == null) lunchStart = Calendar.getInstance()
+                setTime(lunchStart, hourOfDay, minute)
+                lunchStartButton.text = timeFormater.format(lunchStart?.time)
+            }
+        }
+    }
+
+    private fun setTime(calendar: Calendar?, hourOfDay: Int, minute: Int) {
+        calendar?.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar?.set(Calendar.MINUTE, minute)
+        calendar?.set(Calendar.SECOND, 0)
     }
 
 }
 
-class DatePickerFragment : DialogFragment() {
+class TimePickerFragment : DialogFragment(),
+        TimePickerDialog.OnTimeSetListener {
+
+    var listener: TimePickedListener? = null
+    var id: Int? = null
+
+    companion object {
+        fun create(id: Int): TimePickerFragment {
+            val bundle = Bundle()
+            bundle.putInt("picker_id", id)
+            val fragment = TimePickerFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val mainActivity = activity as WorkDayActivity
         val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
 
-        return DatePickerDialog(mainActivity, mainActivity, year, month, day)
+        id = arguments.getInt("picker_id")
+
+        return TimePickerDialog(activity, this, hourOfDay, minute, true)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        listener = context as WorkDayActivity
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        id?.let { listener?.timePicked(it, hourOfDay, minute) }
     }
 }
 
-class TimePickerFragment : DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val mainActivity = activity as WorkDayActivity
-        val cal = Calendar.getInstance()
-        val hour = cal.get(Calendar.HOUR_OF_DAY)
-        val second = cal.get(Calendar.SECOND)
-
-        return TimePickerDialog(mainActivity, mainActivity, hour, second, true)
-    }
+interface TimePickedListener {
+    fun timePicked(id: Int, hourOfDay: Int, minute: Int)
 }
